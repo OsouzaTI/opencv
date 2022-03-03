62d74221ee77374034d7468968859bad341aecb0
Ozéias Souza <44115785+OsouzaTI@users.noreply.github.com>
2022-03-03 15:27:46 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2, numpy as np
+
+
+img = cv2.imread('m10.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.bitwise_not(gray)
+cv2.imshow('gray', gray)
+
+coords = np.column_stack(np.where(gray > 0))
+angle = cv2.minAreaRect(coords)[-1]
+print(angle)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
